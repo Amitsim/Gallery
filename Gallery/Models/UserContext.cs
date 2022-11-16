b637d127a4eb48af3419df85a2f3d4912fece1a8
Student <Student@660134MB.COM>
2022-11-16 13:46:43 +0200

    added project

---
+++ Gallery/Models/UserContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gallery.Models
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite(@"Data source=C:\Temp\Demo.db");
+      
+    }
+}
