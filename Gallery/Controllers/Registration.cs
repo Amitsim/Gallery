82868c8f1f3688ae16235fc557d8a57374e532ce
Student <Student@660134MB.COM>
2022-11-16 15:10:32 +0200

    BABA

--- Gallery/Controllers/Registration.cs
+++ Gallery/Controllers/Registration.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Gallery.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,16 @@ namespace Gallery.Controllers
         }
         public IActionResult Register()
         {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            using (var db = new UserContext())
+            {
+                db.Add(user);
+                db.SaveChanges();
+            }
             return View();
         }
     }
